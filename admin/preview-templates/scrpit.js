4cb27dbdc8c69e354754250817687f59dbfd0bc9
Darkst13 <103934915+Darkst13@users.noreply.github.com>
2022-06-23 15:11:57 +0200

    S

--- admin/preview-templates/scrpit.js
+++ admin/preview-templates/scrpit.js
@@ -9,7 +9,7 @@ $(window).scroll(function() {
 });
 
   $(function() {
-    $('.scroll-down').click (function() {
+    $('.arrow').click (function() {
       $('html, body').animate({scrollTop: $('section.ok').offset().top }, 'slow');
       return false;
     });
