277589afd1e96cb8a3d2396800daa6f794759ab2
Darkst13 <103934915+Darkst13@users.noreply.github.com>
2022-06-24 17:22:56 +0200

    s

--- admin/preview-templates/scrpit.js
+++ admin/preview-templates/scrpit.js
@@ -14,34 +14,3 @@ $(function() {
     return false;
   });
 });
-
-var randomImages = (function(){
-  'use strict';
-  return{
-    init: function(){
-      this.events();// init events
-    },
-    events: function(){
-      /* random images snippet*/
-      var images = [
-        'https://i.redd.it/sj7vi0meld591.jpg',
-        'https://i.redd.it/44fcdej5wy591.jpg',
-        'https://i.redd.it/xi3ymfr4myu81.jpg'
-      ];
-      // create style tag in head
-      var s = document.createElement('style');
-      // add id
-      s.id = 'random-Img';
-      // type css
-      s.type = 'text/css';
-      // append in head
-      document.head.appendChild(s);
-      // math random
-      var r = images[Math.floor(Math.random() * images.length)];
-      // add into style
-      s.textContent = '#rand-images{background-image:url('+r+') no-repeat center center fixed;background-size:cover;-moz-background-size:cover;}';
-    }
-  };
-})();
-// Ready for the war
-randomImages.init();
